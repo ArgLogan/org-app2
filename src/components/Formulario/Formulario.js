c914ebb0ef0f900d05225ba5ad3fd5057b64f932
JJRoma <jj_roma@yahoo.ccom.ar>
2023-04-24 22:26:17 -0300

    24042023-002

--- src/components/Formulario/Formulario.js
+++ src/components/Formulario/Formulario.js
@@ -1,21 +1,49 @@
+import { useState } from "react"
 import "./Formulario.css"
 import CampoTexto from "../CampoTexto"
 import ListaOpciones from "../ListaOpciones"
 import Boton from "../Boton"
 
 const  Formulario = ()=>{  
+     const [nombre, actualizarNombre] = useState("")
+     const [puesto, actualizarPuesto] = useState("")
+     const [foto, actualizarFoto] = useState("")
+
      const manejarEnvio =(e) =>{
           e.preventDefault()
-          console.log(e)
+          let datosAEviar = {
+               nombre: nombre,
+               puesto: puesto,
+               foto: foto
+          }
+          console.log(datosAEviar)
 
      } 
      
      return <section className="formulario">
              <form onSubmit={manejarEnvio}>
                <h2>Rellena el formulario para crear el colaborador.</h2>
-               <CampoTexto titulo="Nombre" placeholder="Ingresar nombre" required={true}/>
-               <CampoTexto titulo="Puesto" placeholder="Ingresar puesto" required/>
-               <CampoTexto titulo="Foto" placeholder="Ingresar emlace de foto" required/>
+               <CampoTexto 
+                    titulo="Nombre" 
+                    placeholder="Ingresar nombre" 
+                    required={true} 
+                    valor={nombre} 
+                    actualizarValor={actualizarNombre}
+               />
+               <CampoTexto 
+                    titulo="Puesto" 
+                    placeholder="Ingresar puesto" 
+                    required
+                    valor={puesto} 
+                    actualizarValor={actualizarPuesto}
+               />
+               <CampoTexto 
+                    titulo="Foto" 
+                    placeholder="Ingresar emlace de foto" 
+                    required
+                    valor={foto} 
+                    actualizarValor={actualizarFoto }
+               />
                <ListaOpciones/>
                <Boton>
                     crear
--- src/components/MiOrg/index.js
+++ src/components/MiOrg/index.js
@@ -1,9 +1,22 @@
+//import { useState } from "react"
 import "./MiOrg.css"
 
-const MiOrg = () =>{
+const MiOrg = (props) =>{
+    //Estado - hooks
+    //UseState
+    //useState()
+    //const [nombreVrible, funcionActualizar] = useState(valorInicial)
+    //const [nombre,actualizarNombre] = useState("dato")
+    //const [mostrar,actualizarMostrar] = useState(true)
+    
+    /*const manejarClick = () =>{
+        props.actualizarMostrar
+
+    }*/
+
     return <section className="org-section">
         <h3 className="title">Mi organización</h3>
-        <img src="/img/add.png" alt="add"/>
+        <img src="/img/add.png" alt="add" onClick={props.cambiarMostrar}/>
     </section>
 }
 export default MiOrg 
